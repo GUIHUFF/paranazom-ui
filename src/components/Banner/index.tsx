929f8ed2c4456d22f206f6d5f20bfe0db3c71e65
Guilherme Huff <ghuff@alunos.utfpr.edu.br>
2022-12-10 18:15:34 -0300

    responsive banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -12,11 +12,11 @@ export default function Banner ({ position, img, textStrong, textThin } : Banner
   return (
     <div 
       className={`flex items-center justify-center gap-4 my-10 
-        ${position === 'left' ? 'flex-row' : 
-          position === 'right' ? 'flex-row-reverse' :
-          position === 'top' ? 'flex-col' :
-          position === 'bottom' ? 'flex-col-reverse' : 'flex-row'
-        }`}>
+        ${position === 'left' ? 'sm:flex-row' : 
+          position === 'right' ? 'sm:flex-row-reverse' :
+          position === 'top' ? 'sm:flex-col' :
+          position === 'bottom' ? 'sm:flex-col-reverse' : 'sm:flex-row'
+        } flex-col`}>
       <img src={img} className="w-[250px] h-[300px]"/>
       <h1 className="text-left text-3xl"><span className="font-bold">{textStrong}</span> <br/><span className="font-thin">{textThin}</span></h1>
     </div> 
