a3d0b7edcf7b04082e5f07823f68c6af1a8edaeb
Guilherme Huff <ghuff@alunos.utfpr.edu.br>
2022-12-12 07:56:53 -0300

    quantidade de prodtos

--- src/pages/Produtos/index.tsx
+++ src/pages/Produtos/index.tsx
@@ -117,6 +117,7 @@ export default function Produtos () {
                 price={product.price}
                 title={product.name}
                 onClick={() => navigate(`/produto/${product.product_id}`)}
+                amount={product.amount}
               />
             )
           })}
